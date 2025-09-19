605debae90fdfd58dff2f7d43aadfba29470f117
Mohawk <Mohawk@MICHAEL>
2025-09-19 16:36:57 -0400

    Added a rectangle that follows the mouse

--- 2d-project-test/Game.cs
+++ 2d-project-test/Game.cs
@@ -18,7 +18,9 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("This is a 2D test");
+            Window.SetSize(800, 800);
+            Window.TargetFPS = 60;
         }
 
         /// <summary>
@@ -26,7 +28,20 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            // clear the screen
+            //Window.ClearBackground(Color.LightGray);
 
+            // draw a rectangle
+            Draw.FillColor = Color.Blue;
+
+            Color orange = new Color(255, 165, 0);
+            Draw.LineColor = orange;
+            Draw.LineSize = 10.0f;
+
+            //Vector2 rectanglePosition = new Vector2(300, 200);
+            Vector2 rectangleSize = new Vector2(200, 100);
+
+            Draw.Rectangle(Input.GetMousePosition() - rectangleSize/2, rectangleSize);
         }
     }
 
